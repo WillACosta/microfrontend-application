4c525712da9005567d2d4ad4908815be9e6969da
willACosta <williamamaral521@gmail.com>
2023-06-07 17:32:27 -0300

    style: add final dot in container header placeholder

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
   return (
     <div>
-      <strong>Add Header here</strong>
+      <strong>Add Header here.</strong>
       <hr />
       <MarketingApp />
     </div>
