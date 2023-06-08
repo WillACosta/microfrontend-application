7fc94cafeb872203d6d54152344da66cd619c966
willACosta <williamamaral521@gmail.com>
2023-06-08 16:28:48 -0300

    refactor: add header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
+
+import Header from "./components/Header"
 import MarketingApp from "./components/MarketingApp"
 
 export default () => {
   return (
-    <div>
-      <strong>Container App Header</strong>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
